bef323d15fcf5edf0ccad6055b2d4ef45f3994df
MuhJamaaluddinIrsyaad <mjirsyaad14@gmail.com>
2021-03-03 14:35:08 +0700

    cuaks

--- app.js
+++ app.js
@@ -63,15 +63,11 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
-app.all('*', function(req, res, next) {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-  res.header('Access-Control-Allow-Headers', 'Content-Type');
-  if ('OPTIONS' == req.method) {
-  res.sendStatus(200);
-  } else {
-    next();
-  }
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+  next();
 });
 
 module.exports = app;
