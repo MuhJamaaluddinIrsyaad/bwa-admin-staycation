5602307d187a9a9137b5895af77df3fd9c3b7a6e
MuhJamaaluddinIrsyaad <mjirsyaad14@gmail.com>
2021-03-03 14:52:35 +0700

    bodong

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ var apiRouter = require('./routes/api');
 var app = express();
 
 app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Origin", "http://localhost:3000");
   res.header("Access-Control-Allow-Credentials", "true");
   res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
   res.header("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers");
