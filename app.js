886c0d7d9cafc7672344ae7b10818bb72fc0a523
MuhJamaaluddinIrsyaad <mjirsyaad14@gmail.com>
2021-03-03 14:04:24 +0700

    3

--- app.js
+++ app.js
@@ -26,7 +26,7 @@ var app = express();
 app.all('*', function(req, res, next) {
   res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-  res.header('Access-Control-Allow-Headers', 'Content-Type');
+  res.header('Access-Control-Allow-Headers', '*');
   if ('OPTIONS' == req.method) {
   res.sendStatus(200);
   } else {
