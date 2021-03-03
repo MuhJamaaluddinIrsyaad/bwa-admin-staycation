8993de81c05a6b14ced2ce76f30535e6c6966c43
MuhJamaaluddinIrsyaad <mjirsyaad14@gmail.com>
2021-03-03 14:23:10 +0700

    cok

--- app.js
+++ app.js
@@ -66,7 +66,7 @@ app.use(function(err, req, res, next) {
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Credentials", "true");
-  res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+  res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, PATCH, DELETE");
   res.header("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers");
   next();
 });
