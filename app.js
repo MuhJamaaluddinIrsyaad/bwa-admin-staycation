d9fafe451ac105f29ed6366fefac097da2ae1ca5
MuhJamaaluddinIrsyaad <mjirsyaad14@gmail.com>
2021-03-03 14:41:12 +0700

    mboh

--- app.js
+++ app.js
@@ -26,7 +26,7 @@ var app = express();
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+  res.header("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers");
   next();
 });
 
