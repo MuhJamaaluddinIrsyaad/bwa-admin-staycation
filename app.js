6d16864c5807677d5430591c243530b6c1a88ab4
MuhJamaaluddinIrsyaad <mjirsyaad14@gmail.com>
2021-03-03 13:36:12 +0700

    update access control fix

--- app.js
+++ app.js
@@ -28,9 +28,12 @@ var app = express();
 //   next();
 // });
 
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Headers","*")
-})
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+  next();
+});
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
